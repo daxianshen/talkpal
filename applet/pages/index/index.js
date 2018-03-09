a908b3aff346ffb59a065fab9287097ea642d97a
zhongxia <418760129@qq.com>
2018-03-09 17:46:08 +0800

    add index display

--- applet/pages/index/index.js
+++ applet/pages/index/index.js
@@ -3,9 +3,28 @@
 var app = getApp()
 Page({
   data: {
-    motto: '简约，而不简单',
-    userInfo: {},
-    windowHeight:0
+    movies: [
+      { url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg' },
+      { url: 'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg' },
+      { url: 'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg' },
+      { url:'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg' }
+    ],
+    banners: [
+      { url: 'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg' },
+      { url: 'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg' },
+      { url: 'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg' },
+      { url: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg' }
+    ],
+   sections: [
+      { url:'http://img04.tooopen.com/images/20130712/tooopen_17270713.jpg',title:"公司介绍" },
+      { url:'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg',title: "公司介绍" },
+      { url:'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg', title: "公司介绍" },
+      { url: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg', title: "公司介绍" },
+      { url: 'http://img04.tooopen.com/images/20130617/tooopen_21241404.jpg', title: "公司介绍" },
+      { url: 'http://img04.tooopen.com/images/20130701/tooopen_20083555.jpg', title: "公司介绍" },
+      { url: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg', title: "公司介绍" },
+      { url: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg', title: "公司介绍" }
+    ]
   },
   //事件处理函数
   bindViewTap: function() {
