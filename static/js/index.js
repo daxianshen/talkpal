ec495d0206c9081159ae69ec0ab9c35bbd54d98b
daxianshen <m13790808952@163.com>
2018-08-03 16:42:51 +0800

    邀请链接错误返回处理更新

--- static/js/index.js
+++ static/js/index.js
@@ -1565,13 +1565,13 @@ var app = new Vue({
 					error:function (e,b,c) {
 						console.log(e,b,c);
                         var errorText = JSON.parse(e.responseText);
-                        if (errorText.errors.phone_number) {
-                            if(errorText.errors.phone_number[0] === "Registered"){
-                                alert("该号码已经注册,请打开或下载 App 直接登陆");
-                                _this.succeedBol = true;
-                                _this.CodeBol = false;
-                            }
-                        }
+                        if (errorText.message === 'The phone number has been registered') {
+							alert("该号码已经注册,请打开或下载 App 直接登陆");
+							_this.succeedBol = true;
+							_this.CodeBol = false;
+                        }else if(errorText.message === 'Incorrect verification code'){
+                            alert('您输入的验证码有误，请等待重新获取');
+						}
                         if (errorText.errors.invitee_phone_number) {
                             if(errorText.errors.invitee_phone_number[0] === "has already been taken"){
                                 alert("您已成功接受邀请");
@@ -1579,11 +1579,11 @@ var app = new Vue({
                                 _this.CodeBol = false;
                             }
                         }
-                        if (errorText.errors.verify) {
-                            if (errorText.errors.verify[0] === 'Incorrect verification code') {
-                                alert('您输入的验证码有误，请等待重新获取');
-                            }
-                        }
+                        // if (errorText.errors.verify) {
+                        //     if (errorText.errors.verify[0] === 'Incorrect verification code') {
+                        //         alert('您输入的验证码有误，请等待重新获取');
+                        //     }
+                        // }
                     }
 				});
 				this.AcceptTheInvitation = false;
